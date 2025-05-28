cd64fa25ccd4a7c2f304643ddd487efba29592c9
arlinda-dwi <arlindabelajar44@students.unnes.ac.id>
2025-05-28 14:34:11 +0700

    Update 2_halamanketiga.py

--- pages/2_halamanketiga.py
+++ pages/2_halamanketiga.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_tittle="Halaman Ketiga")
-st.tittle("Tes")
+st.set_page_config(page_title="Halaman Ketiga")
+st.title("Tes")
 st.sidebar.header("Halaman Ketiga")
